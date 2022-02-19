5e779ad2d6ce644dd3cfbc1ece7a91eeab19a53d
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-02-18 20:36:22 -0500

    Improved AIActionPlanner

--- Assets/Scripts/Tank/AI/AIActionPlanner.cs
+++ Assets/Scripts/Tank/AI/AIActionPlanner.cs
@@ -14,6 +14,39 @@ public class AIActionPlanner : MonoBehaviour
 
     public event Action<Vector3, int> OnActionPlanner;
 
+    private class InitializedValues
+    {
+        internal int _stepsLength;
+        internal int _direction;
+        internal Vector3 _destination;
+
+        internal InitializedValues(int stepsLength, int direction, Vector3 destination)
+        {
+            _stepsLength = stepsLength;
+            _direction = direction;
+            _destination = destination;
+        }
+    }
+    private class UpdatedValues
+    {
+        internal Vector3 _currentTilePos;
+        internal Vector3 _nextTilePos;
+        internal Vector3 _aboveNextTilePos;
+        internal float _desiredDirection;
+        internal float _y;
+
+        internal UpdatedValues(Vector3 currentTilePos, float desiredDirection, float y)
+        {
+            _currentTilePos = currentTilePos;
+            _aboveNextTilePos = new Vector3(0, 0.5f, 0);
+            _desiredDirection = desiredDirection;
+            _y = y;
+        }
+    }
+
+    private InitializedValues _initializedValues;
+    private UpdatedValues _updatedValues;
+
 
     private void Awake()
     {
@@ -32,73 +65,47 @@ public class AIActionPlanner : MonoBehaviour
     {
         _turnController.OnTurnChanged -= OnTurnChanged;
     }
-
-    private void InitializeAIMovementParameters(out int stepsLength, out int direction, out Vector3 destination)
-    {
-        stepsLength = UnityEngine.Random.Range(5, 15);
-        destination = Vector3.zero;
-        direction = 0;
-    }
-
+   
     private void OnTurnChanged(TurnState arg1, CameraMovement arg2)
     {
         if(arg1 == _playerTurn.MyTurn)
         {
             _rayCasts.CastRays(Vector3.zero, Vector3.zero);
-
-            int stepsLength = UnityEngine.Random.Range(5, 15);
-            Vector3 destination = Vector3.zero;
-            int direction = 0;
-
+            _initializedValues = new InitializedValues(UnityEngine.Random.Range(1, 10), 0, Vector3.zero);
 
             if (_rayCasts.DownHit.collider?.tag == Tags.Tile)
-            {              
-                Vector3 currentTilePos = _rayCasts.DownHit.collider.transform.position;
-                Vector3 nextTilePos;
-                float desiredDirection = UnityEngine.Random.Range(-1, 2);
-                float y = 0;
+            {
+                _updatedValues = new UpdatedValues(_rayCasts.DownHit.collider.transform.position,
+                    _rayCasts.DownHit.collider.name == Names.RS ? 1:
+                    _rayCasts.DownHit.collider.name == Names.LS ? -1 : UnityEngine.Random.Range(-1, 2), 0);
 
-                for (float i = 0.5f; i < stepsLength; i += 0.5f)
+                for (float i = 0.5f; i < _initializedValues._stepsLength; i += 0.5f)
                 {
-                    nextTilePos = currentTilePos - new Vector3(desiredDirection > 0 ? i : -i , y, 0);
+                    _updatedValues._nextTilePos = _updatedValues._currentTilePos - new Vector3(_updatedValues._desiredDirection > 0 ? i : -i , _updatedValues._y, 0);
 
-                    if(desiredDirection > 0)
+                    if(_updatedValues._desiredDirection > 0)
                     {
-                        if (LS(new Vector3(nextTilePos.x, nextTilePos.y + 0.5f, nextTilePos.z)))
-                        {
-                            print("LS");
-                            y += -0.5f;
-                            nextTilePos = currentTilePos - new Vector3(desiredDirection > 0 ? i : -i, y, 0);
-                        }
-
-                        if (RS(nextTilePos))
-                        {
-                            print("RS");
-                            y -= -0.5f;
-                        }
+                        Conditions<bool>.Compare(Slope(_updatedValues._nextTilePos + _updatedValues._aboveNextTilePos, Names.LS), Slope(_updatedValues._nextTilePos, Names.RS),
+                            delegate { IncreaseY(i); },
+                            delegate { DecreaseY(); },
+                            null,
+                            null);
                     }
                     else
                     {
-                        if (LS(nextTilePos))
-                        {
-                            print("LS");
-                            y -= -0.5f;
-                        }
-
-                        if (RS(new Vector3(nextTilePos.x, nextTilePos.y + 0.5f, nextTilePos.z)))
-                        {
-                            y += -0.5f;
-                            nextTilePos = currentTilePos - new Vector3(desiredDirection > 0 ? i : -i, y, 0);
-                        }
+                        Conditions<bool>.Compare(Slope(_updatedValues._nextTilePos, Names.LS), Slope(_updatedValues._nextTilePos + _updatedValues._aboveNextTilePos, Names.RS),
+                            delegate { DecreaseY(); },
+                            delegate { IncreaseY(i); },
+                            null,
+                            null);
                     }
 
-                    print(nextTilePos);
-
-                    if (_changeTiles.HasTile(nextTilePos) && IsNextPositionAvailable(_tilesGenerator.TilesDict[nextTilePos].name))
+                    if (_changeTiles.HasTile(_updatedValues._nextTilePos) && IsNextPositionAvailable(_tilesGenerator.TilesDict[_updatedValues._nextTilePos].name))
                     {
-                        destination = nextTilePos;
-                        direction = desiredDirection > 0 ? 1 : -1;
+                        _initializedValues._destination = _updatedValues._nextTilePos;
+                        _initializedValues._direction = _updatedValues._desiredDirection > 0 ? 1 : -1;
                     }
+                    
                     else
                     {
                         break;
@@ -106,8 +113,19 @@ public class AIActionPlanner : MonoBehaviour
                 }
             }
 
-            StartCoroutine(InvokeOnActionPlanner(destination, direction));
+            StartCoroutine(InvokeOnActionPlanner(_initializedValues._destination, _initializedValues._direction));
         }
+    }
+
+    private void IncreaseY(float i)
+    {
+        _updatedValues._y += -0.5f;
+        _updatedValues._nextTilePos = _updatedValues._currentTilePos - new Vector3(_updatedValues._desiredDirection > 0 ? i : -i, _updatedValues._y, 0);
+    }
+
+    private void DecreaseY()
+    {
+        _updatedValues._y -= -0.5f;
     }
 
     IEnumerator InvokeOnActionPlanner(Vector3 destination, int direction)
@@ -117,14 +135,9 @@ public class AIActionPlanner : MonoBehaviour
         OnActionPlanner?.Invoke(destination, direction);
     }
 
-    private bool LS(Vector3 nextTilePos)
+    private bool Slope(Vector3 nextTilePos, string name)
     {
-        return _tilesGenerator.TilesDict.ContainsKey(nextTilePos) && _tilesGenerator.TilesDict[nextTilePos].name == Names.LS;
-    }
-
-    private bool RS(Vector3 nextTilePos)
-    {
-        return _tilesGenerator.TilesDict.ContainsKey(nextTilePos) && _tilesGenerator.TilesDict[nextTilePos].name == Names.RS;
+        return _tilesGenerator.TilesDict.ContainsKey(nextTilePos) && _tilesGenerator.TilesDict[nextTilePos].gameObject != null && _tilesGenerator.TilesDict[nextTilePos].name == name;
     }
 
     private bool IsNextPositionAvailable(string name)
