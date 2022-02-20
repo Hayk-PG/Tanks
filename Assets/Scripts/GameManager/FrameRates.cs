4a691b218e8b04e164e7feee7438e302fd619085
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-02-20 17:06:53 -0500

    Added framerates controller, add parachute sound

---
+++ Assets/Scripts/GameManager/FrameRates.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FrameRates : MonoBehaviour
+{
+    [SerializeField]
+    private int _frameRate;
+
+    [Range(0,2)]
+    [SerializeField]
+    private int _vSyncCount;
+
+
+    private void Awake()
+    {
+        Application.targetFrameRate = _frameRate;
+        QualitySettings.vSyncCount = _vSyncCount;
+    }
+}
