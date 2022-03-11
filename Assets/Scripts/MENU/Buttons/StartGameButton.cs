26a9af36dd12f05f774286c77ac62bf30f774eeb
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-03-11 18:35:26 -0500

    Menu game start button

---
+++ Assets/Scripts/MENU/Buttons/StartGameButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+public class StartGameButton : MonoBehaviour, IOnButtonInteract
+{
+    public GlobalOnButtonInteract GlobalOnButtonInteract { get; set; }
+
+    public void StartTheGame()
+    {
+        MySceneManager.Instance.LoadScene(MySceneManager.SceneName.Game);
+        GlobalOnButtonInteract?.OnChangeButtonSpriteAndColor(GlobalOnButtonInteract.ButtonState.Clicked);
+    }
+}
---
+++ Assets/Scripts/SceneManager/MySceneManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MySceneManager : MonoBehaviour
+{   
+    public enum SceneName { Menu, Game}
+
+    [HideInInspector]
+    public SceneName _sceneName;
+
+    public static MySceneManager Instance { get; private set; }
+    public string MenuSceneName { get; private set; } = "Menu";
+    public string GameSceneName { get; private set; } = "Game";
+    public Scene CurrentScene { get => SceneManager.GetActiveScene(); }
+
+
+
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(SceneName sceneName)
+    {
+        switch (sceneName)
+        {
+            case SceneName.Menu: SceneManager.LoadScene(MenuSceneName); break;
+            case SceneName.Game: SceneManager.LoadScene(GameSceneName); break;
+        }
+    }
+}
