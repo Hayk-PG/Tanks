4344d32be0ad0df431a97f7d59c411edb2c18e35
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-04-21 05:52:49 -0400

    Updated the previous one

--- Assets/Scripts/Player/OnlinePlayer/PhotonPlayerShootRPC.cs
+++ Assets/Scripts/Player/OnlinePlayer/PhotonPlayerShootRPC.cs
@@ -3,13 +3,6 @@ using UnityEngine;
 
 public class PhotonPlayerShootRPC : PhotonPlayerBaseRPC
 {
-    [SerializeField] BulletController _bulletController;
-    public BulletController BulletController
-    {
-        get => _bulletController;
-        set => _bulletController = value;
-    }
-
     public void CallShootRPC(float force)
     {
         _photonPlayerController.PhotonView.RPC("ShootRPC", RpcTarget.AllViaServer, force);
