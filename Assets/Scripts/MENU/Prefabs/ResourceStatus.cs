c4bd500f2a065374445f6d397c56834ec16afeff
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-05-31 00:30:29 -0400

    Resources

---
+++ Assets/Scripts/MENU/Prefabs/ResourceStatus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceStatus : MonoBehaviour
+{
+    [SerializeField] private Text _textValue;
+    public int Value
+    {
+        get => int.Parse(_textValue.text);
+        set => _textValue.text = value.ToString();
+    }
+
+
+    private void Awake()
+    {
+        Value = 0;
+    }
+}
---
+++ Assets/Scripts/MENU/TopBar/TopBarResourcesContainer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TopBarResourcesContainer : MonoBehaviour
+{
+    private RectTransform _titleRectTransform;
+    private Vector2 _defaultAnchorMax;
+    private Vector2 _defaultOffsetMax;
+    private CanvasGroup _buttonsContainer;
+
+    private void OnEnable()
+    {
+        if (_titleRectTransform == null)
+        {
+            _titleRectTransform = transform.parent.Find("Title").GetComponent<RectTransform>();
+            _buttonsContainer = transform.parent.Find("ButtonsContainer").GetComponent<CanvasGroup>();
+            _defaultAnchorMax = _titleRectTransform.anchorMax;
+            _defaultOffsetMax = _titleRectTransform.offsetMax;
+            
+        }
+
+        GlobalFunctions.CanvasGroupActivity(_buttonsContainer, false);
+        _titleRectTransform.anchorMax = new Vector2(0.2f, _titleRectTransform.anchorMax.y);
+        _titleRectTransform.offsetMax = Vector2.zero;        
+    }
+
+    private void OnDisable()
+    {
+        GlobalFunctions.CanvasGroupActivity(_buttonsContainer, true);
+        _titleRectTransform.anchorMax = _defaultAnchorMax;
+        _titleRectTransform.offsetMax = _defaultOffsetMax;       
+    }
+}
