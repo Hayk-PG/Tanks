814e48401096969457a662441d4cca7d56467c46
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-06-11 17:31:58 -0400

    TerritoryOccupiedIndicator

---
+++ Assets/Scripts/HUD/Tab_TerOccInd/TerOccIndButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TerOccIndButton : MonoBehaviour
+{
+    private CanvasGroup _canvasGroup;
+    private CanvasGroup _upperTabCanvasGroup;
+    private GameManager _gameManager;
+
+    private bool _isOpened;
+
+    private void Awake()
+    {
+        _canvasGroup = Get<CanvasGroup>.FromChild(gameObject);
+        _upperTabCanvasGroup = Get<CanvasGroup>.From(FindObjectOfType<UpperTab>().gameObject);
+        _gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void OnButtonClick()
+    {
+        if (_gameManager.IsGameStarted)
+        {
+            _isOpened = !_isOpened;
+            GlobalFunctions.CanvasGroupActivity(_upperTabCanvasGroup, !_isOpened);
+            GlobalFunctions.CanvasGroupActivity(_canvasGroup, _isOpened);
+        }
+    }
+}
---
+++ Assets/Scripts/HUD/Tab_TerOccInd/TerritoryOccupiedIndicator.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TerritoryOccupiedIndicator : MonoBehaviour
+{
+    private LevelGenerator _levelGenerator;
+    private GameManager _gameManager;
+    private Transform _tank1Transform;
+    private Transform _tank2Transform;
+
+    [SerializeField] private RectTransform _player1;
+    [SerializeField] private RectTransform _player2;
+    [SerializeField] private Text _textPlayer1Percentage;
+    [SerializeField] private Text _textPlayer2Percentage;
+
+    private float _zeroValue;
+    private float _player1Value;
+    private float _player2Value;
+
+    public int Player1Percentage { get; private set; }
+    public int Player2Percentage { get; private set; }
+
+
+    private void Awake()
+    {
+        _levelGenerator = FindObjectOfType<LevelGenerator>();
+        _gameManager = FindObjectOfType<GameManager>();
+
+        _zeroValue = 1589;
+    }
+
+    private void OnEnable()
+    {
+        _gameManager.OnGameStarted += OnGameStarted;
+    }
+
+    private void OnDisable()
+    {
+        _gameManager.OnGameStarted -= OnGameStarted;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_tank1Transform != null)
+        {
+            _player1Value = Mathf.InverseLerp(_levelGenerator.MapHorizontalEndPoint, _levelGenerator.MapHorizontalStartPoint, _tank1Transform.position.x);
+            _player1.offsetMax = new Vector2(-(_player1Value * _zeroValue), _player1.offsetMax.y);
+            Player1Percentage = Mathf.FloorToInt((_zeroValue - Mathf.Abs(_player1.offsetMax.x)) / _zeroValue * 100);
+            _textPlayer1Percentage.text = Player1Percentage + "%";
+        }
+
+        if (_tank2Transform != null)
+        {
+            _player2Value = Mathf.InverseLerp(_levelGenerator.MapHorizontalStartPoint, _levelGenerator.MapHorizontalEndPoint, _tank2Transform.position.x);
+            _player2.offsetMin = new Vector2((_player2Value * _zeroValue), _player2.offsetMin.y);
+            Player2Percentage = Mathf.FloorToInt((_zeroValue - Mathf.Abs(_player2.offsetMin.x)) / _zeroValue * 100);
+            _textPlayer2Percentage.text = Player2Percentage + "%";
+        }
+    }
+
+    private void OnGameStarted()
+    {
+        _tank1Transform = _gameManager.Tank1?.transform;
+        _tank2Transform = _gameManager.Tank2?.transform;
+    }
+}
