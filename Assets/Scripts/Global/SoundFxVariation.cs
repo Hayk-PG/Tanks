318cdda24142da3701c29752ced6e84a7d937720
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-06-30 22:02:49 -0400

    SoundVariation MuzzleFX

---
+++ Assets/Scripts/Global/SoundFxVariation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SoundFxVariation : MonoBehaviour
+{
+    private AudioSource _audioSRC;
+    [SerializeField] private float _minPitch, _maxPitch;
+    [SerializeField] private float _minVolume, _maxVolume;
+
+
+
+    private void Awake()
+    {
+        _audioSRC = Get<AudioSource>.From(gameObject);
+        Play();
+    }
+
+    private void Play()
+    {
+        _audioSRC.pitch = Random.Range(_minPitch, _maxPitch);
+        _audioSRC.volume = Random.Range(_minVolume, _maxVolume);
+        _audioSRC.Play();
+    }
+}
