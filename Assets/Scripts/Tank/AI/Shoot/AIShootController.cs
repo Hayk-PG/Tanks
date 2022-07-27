b1326a5271850abbe5b0b9e256049a997c7abaf9
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-07-27 17:36:29 -0400

    Minor changes

--- Assets/Scripts/Tank/AI/Shoot/AIShootController.cs
+++ Assets/Scripts/Tank/AI/Shoot/AIShootController.cs
@@ -14,8 +14,16 @@ public class AIShootController : BaseShootController
 
     [SerializeField] private BulletController[] _bulletsPrefab;
     [SerializeField] private int _activeBulletIndex;
+    private bool CanRotateCanon
+    {
+        get
+        {
+            return Converter.AngleConverter(_canon._currentEulerAngleX) < _canon._maxEulerAngleX &&
+                   Converter.AngleConverter(_canon._currentEulerAngleX) > _canon._minEulerAngleX;
+        }
+    }
 
-    
+
     protected override void Awake()
     {
         base.Awake();
@@ -37,24 +45,22 @@ public class AIShootController : BaseShootController
 
     private void Update()
     {
-        if (_player != null) _target = _trajectory.PredictedTrajectory(_player.position, transform.position, 1);
-
         RotateCanon();
     }
 
     public void RotateCanon()
     {
+        if (CanRotateCanon)
+        {
+            _target = _trajectory.PredictedTrajectory(_player.position, transform.position, 1);           
+        }
+
         _lookRot = Quaternion.LookRotation(Vector3.forward, _target);
         _rot = _lookRot * Quaternion.Euler(_canon._rotationStabilizer.x, _canon._rotationStabilizer.y, _canon._rotationStabilizer.z);
         _desiredRotation = Quaternion.Slerp(_desiredRotation, _rot, _canon._rotationSpeed);
         _canon._rotationSpeed = 2 * Time.deltaTime;
         _canon._currentEulerAngleX = _desiredRotation.eulerAngles.x;
-
-        if (Converter.AngleConverter(_canon._currentEulerAngleX) < _canon._maxEulerAngleX && 
-            Converter.AngleConverter(_canon._currentEulerAngleX) > _canon._minEulerAngleX)
-        {
-            _canonPivotPoint.rotation = _desiredRotation;
-        }  
+        _canonPivotPoint.rotation = _desiredRotation;
     }
 
     private void ShootBullet()
