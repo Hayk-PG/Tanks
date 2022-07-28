8428227265796918fc04a3a8423499b25fea357a
Hayk Hunanyan <88951760+HaykFromPautikGames@users.noreply.github.com>
2022-07-27 22:45:54 -0400

    Added more sounds

---
+++ Assets/Scripts/Tank/TankTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class TankTrigger : MonoBehaviour
+{
+    private IDamage _iDamage;
+    private IDestruct _iDestruct;
+    private int _damageValue = 10;
+    private float _time;
+
+    private void Awake()
+    {
+        _iDamage = Get<IDamage>.From(gameObject);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponent<IDestruct>() != null)
+        {
+            _time += Time.deltaTime;
+
+            if (_time >= 0.5f)
+                TakeDamageAndDestroy(other);
+        }
+    }
+
+    private void TakeDamageAndDestroy(Collider other)
+    {
+        _iDamage.Damage(_damageValue);
+        _iDestruct = other.GetComponent<IDestruct>();
+        _iDestruct.Destruct(100, 1000);
+        _time = 0;
+        print("aaa");
+    }
+}
