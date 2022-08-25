61eb1fc6ab6c7d0bb71b549f4e3b9f5bd5b61b87
Hayk Hunanyan <pautikgames@gmail.com>
2022-08-24 23:34:40 -0400

    Added UI sounds

---
+++ Assets/Scripts/MENU/Buttons/Button_Sound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Button_Sound : MonoBehaviour
+{
+    [SerializeField] private int _listIndex;
+    [SerializeField] private int _clipIndex;
+
+
+    public void OnButtonSound()
+    {
+        UISoundController.PlaySound(_listIndex, _clipIndex);
+    }
+}
