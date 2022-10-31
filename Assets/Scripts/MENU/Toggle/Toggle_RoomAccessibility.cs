fb2e014fde7828bb07634bd7293771f280919a71
Hayk Hunanyan <pautikgames@gmail.com>
2022-10-31 00:51:51 -0400

    Added tabs and buttons for matchmake

---
+++ Assets/Scripts/MENU/Toggle/Toggle_RoomAccessibility.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using TMPro;
+
+public class Toggle_RoomAccessibility : BaseSliderLevel<int>
+{
+    [SerializeField] private TMP_InputField _passwordInputField;
+
+    public override void OnSliderValueChanged()
+    {
+        UpdateTitleText(Mathf.FloorToInt(_slider.value));
+        SetPasswordInputFieldInteractability(Mathf.FloorToInt(_slider.value));
+    }
+
+    protected override void Activate()
+    {
+        
+    }
+
+    protected override string Title(string prefix)
+    {
+        return prefix;
+    }
+
+    protected override void UpdateTitleText(int index)
+    {
+        _title.text = Title(index == 0 ? "public" : "private");
+    }
+
+    private void SetPasswordInputFieldInteractability(int index)
+    {
+        _passwordInputField.interactable = index == 0 ? false : true;
+    }
+}
