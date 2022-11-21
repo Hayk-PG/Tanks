6d699b32b3a8b7f796c681b15a94a4b76275e570
Hayk Hunanyan <pautikgames@gmail.com>
2022-11-21 03:28:44 -0500

    added => LobbyGUI script

---
+++ Assets/Scripts/MENU/Tabs/Tab_Lobby/LobbyGUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LobbyGUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _txtTitle;
+    [SerializeField] private Image _imgBackgroundEffect;
+
+    [SerializeField] private string cntntTitle;
+
+    [SerializeField] private Color _clrTitle;
+
+    public Color TitleColor
+    {
+        get
+        {
+            return _txtTitle.color;
+        }
+        set
+        {
+            _txtTitle.color = value;
+            _imgBackgroundEffect.color = value;
+        }
+    }
+
+
+
+    private void Awake()
+    {
+        SetTitleColor(_clrTitle);
+
+        SetText(_txtTitle.text, cntntTitle);
+    }
+
+    private void SetTitleColor(Color color) => TitleColor = color;
+
+    private void SetText(string txt, string content) => _txtTitle.text = content;
+}
