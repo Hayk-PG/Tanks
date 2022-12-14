830d34085d1272f069cedd197b2d5af445e4f8ba
Hayk Hunanyan <pautikgames@gmail.com>
2022-12-14 16:42:13 -0500

    fix => WoodBoxSerializeView

--- Assets/Scripts/GameManager/GameManagerSerializeViews/WoodBoxSerializeView.cs
+++ Assets/Scripts/GameManager/GameManagerSerializeViews/WoodBoxSerializeView.cs
@@ -3,41 +3,26 @@ using UnityEngine;
 
 public class WoodBoxSerializeView : BaseGameManagerSerializeView
 {
-    private float _randomDestroyTime = 0;
-    private Vector3 _rbVelocity = Vector3.zero;
-    private Vector3 _rbPosition = Vector3.zero;
-    private Quaternion _rbRotation = Quaternion.identity;
-
-
     protected override void Write(PhotonStream stream)
     {
         if (_woodenBoxSerializer.ParachuteWithWoodBoxController != null)
         {
-            _randomDestroyTime = _woodenBoxSerializer.ParachuteWithWoodBoxController.RandomDestroyTime;
-            _rbVelocity = _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.velocity;
-            _rbRotation = _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.rotation;
-            _rbPosition = _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.position;
+            stream.SendNext(_woodenBoxSerializer.ParachuteWithWoodBoxController.RandomDestroyTime);
+            stream.SendNext(_woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.velocity);
+            stream.SendNext(_woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.position);
+            stream.SendNext(_woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.rotation);          
         }
-
-        stream.SendNext(_randomDestroyTime);
-        stream.SendNext(_rbVelocity);
-        stream.SendNext(_rbRotation);
-        stream.SendNext(_rbPosition);
     }
 
     protected override void Read(PhotonStream stream, PhotonMessageInfo info)
     {
-        _randomDestroyTime = (float)stream.ReceiveNext();
-        _rbVelocity = (Vector3)stream.ReceiveNext();
-        _rbRotation = (Quaternion)stream.ReceiveNext();
-        _rbPosition = (Vector3)stream.ReceiveNext();
-
         if (_woodenBoxSerializer.ParachuteWithWoodBoxController != null)
         {
-            _woodenBoxSerializer.ParachuteWithWoodBoxController.RandomDestroyTime = _randomDestroyTime;
-            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.velocity = _rbVelocity;
-            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.rotation = _rbRotation;
-            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.position = _rbPosition + _rbVelocity * Lag(info);
+            _woodenBoxSerializer.ParachuteWithWoodBoxController.RandomDestroyTime = (float)stream.ReceiveNext();
+            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.velocity = (Vector3)stream.ReceiveNext();
+            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.position = (Vector3)stream.ReceiveNext();
+            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.rotation = (Quaternion)stream.ReceiveNext();
+            _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.position += _woodenBoxSerializer.ParachuteWithWoodBoxController.RigidBody.velocity * Lag(info);
         }
     }
 }
