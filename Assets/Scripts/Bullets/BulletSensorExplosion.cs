01edeedf3bf363622cf72ff3443a8495d49bc080
Hayk Hunanyan <pautikgames@gmail.com>
2022-12-16 04:49:24 -0500

    fix => bullet autodestroy on zero rotation

--- Assets/Scripts/Bullets/BulletSensorExplosion.cs
+++ Assets/Scripts/Bullets/BulletSensorExplosion.cs
@@ -4,26 +4,44 @@ public class BulletSensorExplosion : BulletExplosion
 {
     protected override void OnEnable()
     {
-        if (_iBulletLimit != null)
-            _iBulletLimit.OnExplodeOnLimit += OnExplodeOnLimit;
-
-        if (_iBulletLimit != null && _iBulletSensor != null)
-            _iBulletLimit.OnDestroyTimeLimit += delegate { Hit(default); };
-
         if (_iBulletSensor != null)
             _iBulletSensor.OnHit += Hit;
+
+        if (_iBulletLimit != null)
+        {
+            _iBulletLimit.OnExplodeOnLimit += OnExplodeOnLimit;
+            _iBulletLimit.OnDestroyTimeLimit += delegate { Hit(default); };
+        }
+
+        if(_iBulletVelocity != null)
+        {
+            _iBulletVelocity.OnBulletVelocity += delegate (BulletController.VelocityData velocityData)
+            {
+                if (velocityData._rigidBody.rotation == Quaternion.Euler(0, 0, 0))
+                    Hit(default);
+            };
+        }
     }
 
     protected override void OnDisable()
     {
-        if (_iBulletLimit != null)
-            _iBulletLimit.OnExplodeOnLimit -= OnExplodeOnLimit;
-
-        if (_iBulletLimit != null && _iBulletSensor != null)
-            _iBulletLimit.OnDestroyTimeLimit -= delegate { Hit(default); };
-
         if (_iBulletSensor != null)
             _iBulletSensor.OnHit -= Hit;
+
+        if (_iBulletLimit != null)
+        {
+            _iBulletLimit.OnExplodeOnLimit -= OnExplodeOnLimit;
+            _iBulletLimit.OnDestroyTimeLimit -= delegate { Hit(default); };
+        }
+
+        if (_iBulletVelocity != null)
+        {
+            _iBulletVelocity.OnBulletVelocity -= delegate (BulletController.VelocityData velocityData)
+            {
+                if (velocityData._rigidBody.rotation == Quaternion.Euler(0, 0, 0))
+                    Hit(default);
+            };
+        }
     }
 
     private void Hit(RaycastHit hit)
