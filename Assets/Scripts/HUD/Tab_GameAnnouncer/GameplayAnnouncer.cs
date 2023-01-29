05a6f22a638b6dca2d15bcc78671cd1993b9e4b8
Hayk Hunanyan <pautikgames@gmail.com>
2023-01-29 02:28:15 -0500

    added => new font

--- Assets/Scripts/HUD/Tab_GameAnnouncer/GameplayAnnouncer.cs
+++ Assets/Scripts/HUD/Tab_GameAnnouncer/GameplayAnnouncer.cs
@@ -20,12 +20,12 @@ public class GameplayAnnouncer : BaseAnnouncer
 
         if (coroutineCallCounts <= 1)
         {
-            TextAnnouncement(0, "Ready?", true);
+            TextAnnouncement(0, GlobalFunctions.TextWithColorCode("#FF7700", "Ready?"), true);
             SoundController.MusicSRCVolume(SoundController.MusicVolume.Down);
             SoundController.PlaySound(0, Indexes.Combat_Announcer_Male_Effect_Ready, out float clipLength);
             yield return new WaitForSeconds(clipLength);
             TextAnnouncement(0, "", false);
-            TextAnnouncement(0, "Go!", true);
+            TextAnnouncement(0, GlobalFunctions.TextWithColorCode("#9DD70D", "Go"), true);
             OnGameStartAnnouncement?.Invoke();
             SoundController.PlaySound(0, Indexes.Combat_Announcer_Male_Effect_Go, out float nextClipLength);
             yield return new WaitForSeconds(nextClipLength);
