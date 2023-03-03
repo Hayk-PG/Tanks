9432559db0cd8165a8d2cc9150cd659599760eaf
Hayk Hunanyan <pautikgames@gmail.com>
2023-03-03 00:20:37 -0500

    modified => AmmoTypeController

--- Assets/Scripts/HUD/BulletsTab/AmmoTypeController.cs
+++ Assets/Scripts/HUD/BulletsTab/AmmoTypeController.cs
@@ -16,12 +16,17 @@ public class AmmoTypeController : MonoBehaviour
     private AmmoTabButton _ammoTabButton;
 
     [SerializeField] [Space]
+    private BaseRemoteControlTarget _remoteControl;
+
+    [SerializeField] [Space]
     private CameraBlur _cameraBlur;
 
     private const string _play = "play";
     private const string _direction = "speed";
-    private float _animatorSpeed;  
-    
+    private float _animatorSpeed;
+
+    public bool IsOpen => _rectTransform.anchoredPosition.x > 0;
+
     public Action<bool> OnInformAboutTabActivityToTabsCustomization { get; set; }
 
 
@@ -31,24 +36,40 @@ public class AmmoTypeController : MonoBehaviour
     private void OnEnable()
     {
         _ammoTabButton.OnAmmoTabActivity += OnAmmoTabActivity;
+
         _ammoTabCustomization.OnAmmoTypeController += OnAmmoTabActivity;
+
+        _remoteControl.onRemoteControlActivity += isActive =>
+        {
+            if (isActive && IsOpen)
+                OnAmmoTabActivity();
+        };
     }
 
     private void OnDisable()
     {
         _ammoTabButton.OnAmmoTabActivity -= OnAmmoTabActivity;
+
         _ammoTabCustomization.OnAmmoTypeController -= OnAmmoTabActivity;
+
+        _remoteControl.onRemoteControlActivity -= isActive =>
+        {
+            if (isActive && IsOpen)
+                OnAmmoTabActivity();
+        };
     }
 
     public void OnAmmoTabActivity()
     {
-        _animatorSpeed = _rectTransform.anchoredPosition.x > 0 ? 1 : -1;
+        _animatorSpeed = IsOpen ? 1 : -1;
         _animator.SetFloat(_direction, _animatorSpeed);
         _animator.SetTrigger(_play);
 
-        OnInformAboutTabActivityToTabsCustomization?.Invoke(_rectTransform.anchoredPosition.x > 0);
-        _cameraBlur.ScreenBlur(_rectTransform.anchoredPosition.x > 0);
-        PlaySoundFx(_rectTransform.anchoredPosition.x > 0 ? 0 : 1);
+        OnInformAboutTabActivityToTabsCustomization?.Invoke(IsOpen);
+
+        _cameraBlur.ScreenBlur(IsOpen);
+
+        PlaySoundFx(IsOpen ? 0 : 1);
     }
 
     public void PlaySoundFx(int clipIndex)
