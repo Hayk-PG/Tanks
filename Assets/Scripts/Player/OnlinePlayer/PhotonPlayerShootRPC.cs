47942d982f2403d7786e01fd9f3e6dcdf1ba5fbb
Hayk-PG <pautikGames@gmail.com>
2023-03-05 00:32:27 -0500

    update

--- Assets/Scripts/Player/OnlinePlayer/PhotonPlayerShootRPC.cs
+++ Assets/Scripts/Player/OnlinePlayer/PhotonPlayerShootRPC.cs
@@ -4,7 +4,13 @@ public class PhotonPlayerShootRPC : PhotonPlayerBaseRPC, IShoot
 {
     public void LaunchRocket(int id)
     {
-        
+        _photonPlayerController.PhotonView.RPC("LaunchRocketRPC", RpcTarget.AllViaServer, id);
+    }
+
+    [PunRPC]
+    private void LaunchRocketRPC(int id)
+    {
+        _photonPlayerTankController?._shootController.LaunchRocket(id);
     }
 
     public void Shoot(float force)
