5ea8bdcbe874057c1e617dc6392e27892c1eae0a
Hayk Hunanyan <pautikgames@gmail.com>
2023-03-30 05:35:11 -0400

    update

--- Assets/Scripts/HUD/Tab_DropBoxItemSelection/DropBoxSelectionPanelElements/Elements/DropBoxSelectionPanelScores.cs
+++ Assets/Scripts/HUD/Tab_DropBoxItemSelection/DropBoxSelectionPanelElements/Elements/DropBoxSelectionPanelScores.cs
@@ -2,12 +2,9 @@ using UnityEngine;
 
 public class DropBoxSelectionPanelScores : BaseDropBoxSelectionPanelElement
 {
-    private int _randomXp = Random.Range(0, 101);
-
-
     protected override void Use()
     {
-        _data[0] = _randomXp;
+        _data[0] = Random.Range(0, 101);
         _data[1] = NegativePrice;
 
         DropBoxSelectionHandler.RaiseEvent(DropBoxItemType.XpUpgrade, _data);
