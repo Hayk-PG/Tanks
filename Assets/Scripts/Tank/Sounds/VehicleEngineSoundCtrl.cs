967e4a53d13a79294deaa1557a758c02d40d20e0
Hayk-PG <pautikGames@gmail.com>
2023-04-29 21:36:15 -0400

    Reverted the changes done on Jan 22, 2023 of the VehicleEngineSoundCtrl class

--- Assets/Scripts/Tank/Sounds/VehicleEngineSoundCtrl.cs
+++ Assets/Scripts/Tank/Sounds/VehicleEngineSoundCtrl.cs
@@ -2,94 +2,61 @@
 
 public class VehicleEngineSoundCtrl : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField]
     private AudioSource audioSRC;
-    
-    [SerializeField] [Space]
+
+    [SerializeField]
     private AudioClip _engineSound, _stopSound;
 
     private BaseTankMovement _baseTankMovement;
 
+    private bool _hasStopSoundEnded, _isEngineSoundPlaying, _IsStopSoundPlayingOnBrake, _isEngineSoundPlayingOnMovement;
 
-    private void Awake() => _baseTankMovement = Get<BaseTankMovement>.From(gameObject);
 
-    private void Start() => audioSRC.mute = SoundController.IsSoundMuted;
+    private void Awake()
+    {
+        _baseTankMovement = Get<BaseTankMovement>.From(gameObject);
+    }
 
     private void OnEnable()
     {
-        if (!audioSRC.mute)
-            AddListener();
-
-        SoundController.onAudioSourceMute += OnAudioSourceMute;
+        if (_baseTankMovement != null) _baseTankMovement.OnVehicleMove += OnVehicleEngineSound;
     }
 
     private void OnDisable()
     {
-        RemoveListener();
-        SoundController.onAudioSourceMute -= OnAudioSourceMute;
-    }
-
-    private void AddListener()
-    {
-        if (_baseTankMovement != null)
-            _baseTankMovement.OnVehicleMove += OnVehicleEngineSound;
-    }
-
-    private void RemoveListener()
-    {
-        if (_baseTankMovement != null)
-            _baseTankMovement.OnVehicleMove -= OnVehicleEngineSound;
-    }
-
-    private void OnAudioSourceMute(bool isMuted)
-    {
-        audioSRC.mute = isMuted;
-
-        RemoveListener();
-
-        if (!isMuted)
-            AddListener();
+        if (_baseTankMovement != null) _baseTankMovement.OnVehicleMove -= OnVehicleEngineSound;
     }
 
     private void OnVehicleEngineSound(float rpm)
     {
-        Conditions<bool>.Compare(IsSoundEnded(), IsEngineSoundPlaying(), OnSetVehicleSrcVolumeToZero, OnIncreaseVehicleSrcVolume, null, null);
+        _hasStopSoundEnded = audioSRC.clip == _stopSound &&
+                                 audioSRC.time >= _stopSound.length - 0.05f &&
+                                 audioSRC.volume > 0;
+        _isEngineSoundPlaying = audioSRC.clip == _engineSound && audioSRC.volume < 1;
+        _IsStopSoundPlayingOnBrake = rpm == 0 && audioSRC.clip != _stopSound;
+        _isEngineSoundPlayingOnMovement = rpm != 0 && audioSRC.clip != _engineSound;
 
-        Conditions<bool>.Compare(IsTankStopped(rpm), () => { audioSRC.pitch = 1; }, () => { IncreaseVehSrcPitch(rpm); });
 
-        Conditions<bool>.Compare(IsStopSoundPlayingOnBrake(rpm), IsEngineSoundPlayingOnMovement(rpm),
-                                () => SwitchAudioClips(audioSRC, _stopSound, false),
-                                () => SwitchAudioClips(audioSRC, _engineSound, true), null, null);
+        Conditions<bool>.Compare(_hasStopSoundEnded, _isEngineSoundPlaying, OnSetVehicleSrcVolumeToZero, OnIncreaseVehicleSrcVolume, null, null);
+        Conditions<bool>.Compare(rpm == 0, rpm != 0,
+            () => audioSRC.pitch = 1,
+            () => IncreaseVehSrcPitch(rpm)
+            , null, null);
+        Conditions<bool>.Compare(_IsStopSoundPlayingOnBrake, _isEngineSoundPlayingOnMovement,
+            () => SwitchAudioClips(audioSRC, _stopSound, false),
+            () => SwitchAudioClips(audioSRC, _engineSound, true), null, null);
     }
 
-    private bool IsTankStopped(float rpm)
+    private void OnSetVehicleSrcVolumeToZero()
     {
-        return Mathf.Abs(rpm) <= 1;
+        audioSRC.volume = 0;
     }
 
-    private bool IsSoundEnded()
+    private void OnIncreaseVehicleSrcVolume()
     {
-        return audioSRC.clip == _stopSound && audioSRC.time >= _stopSound.length - 0.05f && audioSRC.volume > 0;
+        audioSRC.volume = Mathf.Lerp(audioSRC.volume, 1, 10 * Time.deltaTime);
     }
-
-    private bool IsEngineSoundPlaying()
-    {
-        return audioSRC.clip == _engineSound && audioSRC.volume < 1;
-    }
-
-    private bool IsStopSoundPlayingOnBrake(float rpm)
-    {
-        return IsTankStopped(rpm) && audioSRC.clip != _stopSound;
-    }
-
-    private bool IsEngineSoundPlayingOnMovement(float rpm)
-    {
-        return !IsTankStopped(rpm) && audioSRC.clip != _engineSound;
-    }
-
-    private void OnSetVehicleSrcVolumeToZero() => audioSRC.volume = 0;
-
-    private void OnIncreaseVehicleSrcVolume() => audioSRC.volume = Mathf.Lerp(audioSRC.volume, 1, 10 * Time.deltaTime);
 
     private void IncreaseVehSrcPitch(float rpm)
     {
@@ -104,6 +71,6 @@ public class VehicleEngineSoundCtrl : MonoBehaviour
         src.Stop();
         src.clip = clip;
         src.loop = loop;
-        src.Play();        
+        src.Play();
     }
 }
