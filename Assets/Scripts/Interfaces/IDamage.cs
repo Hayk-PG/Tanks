b22eb2b6f327daa829e1fd20e813c81baaf9024d
Hayk-PG <pautikGames@gmail.com>
2023-05-06 15:30:35 -0400

    Added 'ArmorIgnoringExplosion' script particularly for the Railgun projectile, to ignore any type of armor while applying damage. Also, modified 'Explosion', 'GameManagerBulletSerializer', 'HealthController' scripts to match them with the 'ArmorIgnoringExplosion' script.

--- Assets/Scripts/Interfaces/IDamage.cs
+++ Assets/Scripts/Interfaces/IDamage.cs
@@ -6,7 +6,7 @@ public interface IDamage
     int Health { get; set; }
     Action<BasePlayer, int> OnTakeDamage { get; set; }
     Action<int> OnUpdateHealthBar { get; set; }
-    void Damage(int damage);
-    void Damage(Collider collider, int damage);
+    void Damage(int damage, bool ingoreArmor = false);
+    void Damage(Collider collider, int damage, bool ignoreArmor = false);
     void CameraChromaticAberrationFX();
 }
